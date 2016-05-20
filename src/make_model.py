7a259e55adc4ec192999af6c58e1bf70b4ec5347
ozer <ozerozkahraman@outlook.com>
2016-05-20 18:02:31 +0200

    day3 test2

--- src/make_model.py
+++ src/make_model.py
@@ -9,16 +9,33 @@ from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 
-Conv = lambda num_filt,filt_size: Convolution2D(num_filt, filt_size, filt_size, dim_ordering = 'tf', border_mode = 'same', init = 'glorot_normal')
+Conv = lambda num_filt,filt_size: Convolution2D(num_filt, filt_size, filt_size,\
+dim_ordering = 'tf', border_mode = 'same', init = 'glorot_normal')
+
 Maxpool = lambda size=2: MaxPooling2D((size,size), strides=(size,size))
 Relu = lambda size: Dense(size, activation='relu', init='glorot_normal')
 
 def make_net(num_classes):
 	model = Sequential()
-	model.add(Convolution2D(16, 5, 5, input_shape=(64,64,3), dim_ordering = 'tf', border_mode = 'same'))
+	model.add(Convolution2D(64, 3, 3, input_shape=(64,64,3),\
+	dim_ordering = 'tf', border_mode = 'same', activation='relu'))
+
+	model.add(Conv(32,3))
+	model.add(Dropout(0.25))
+
+	model.add(Conv(64,3))
+	model.add(Maxpool(2))
+	model.add(Dropout(0.25))
+
+	model.add(Conv(64,3))
+	model.add(Maxpool(2))
+	model.add(Dropout(0.25))
 
 	model.add(Flatten())
-	model.add(Relu(16))
+	model.add(Relu(128))
+	model.add(Dropout(0.5))
+	model.add(Relu(128))
+	model.add(Dropout(0.5))
 	model.add(Dense(num_classes, activation='softmax'))
 
 	optim = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
