2a43abf8a2f115d201a2d7448bd842fcee8245a1
ozer <ozerozkahraman@outlook.com>
2016-05-24 15:28:25 +0200

    verbose

--- src/main.py
+++ src/main.py
@@ -109,7 +109,7 @@ for epoch in range(num_epochs):
 	tloss, tacc = 0. , 0.
 	for X_batch, Y_batch in datagen.flow(X_train,Y_train, batch_size=num_batch):
 #		loss, acc = model.train_on_batch(X_batch,Y_batch)
-		history = model.fit(X_batch, Y_batch, batch_size=len(X_batch), nb_epoch=1)
+		history = model.fit(X_batch, Y_batch, batch_size=len(X_batch), nb_epoch=1, verbose=0)
 		loss = history.history['loss'][0]
 		acc = history.history['acc'][0]
 		tloss += loss
