34a9205c737e91c10083f9b8dc51836669bd4eb8
ozer <ozerozkahraman@outlook.com>
2016-05-25 20:21:24 +0200

    7_4(A) (cifar10)

--- src/make_model.py
+++ src/make_model.py
@@ -5,7 +5,7 @@ Created on Thu May 19 11:09:26 2016
 @author: ozer
 """
 from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.core import Flatten, Dense, Dropout, Activation
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 
@@ -23,17 +23,38 @@ Relu = lambda size: Dense(size, activation='relu', init='glorot_normal')
 
 def make_net(num_classes):
 	model = Sequential()
-	model.add(Convolution2D(32,3,3, input_shape=(64,64,3), activation='relu',\
-	border_mode='same',dim_ordering='tf'))
+	model.add(Convolution2D(32, 3, 3, border_mode='same',input_shape=(64,64,3)))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(32, 3, 3))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
 
-	model.add(Convolution2D(32,3,3, activation='relu', border_mode='same',dim_ordering='tf'))
-
-	model.add(Maxpool())
+	model.add(Convolution2D(64, 3, 3, border_mode='same'))
+	model.add(Activation('relu'))
+	model.add(Convolution2D(64, 3, 3))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.25))
 
 	model.add(Flatten())
-	model.add(Relu(128))
-	#model.add(Dropout(0.5))
-	model.add(Dense(num_classes, activation='softmax'))
+	model.add(Dense(512))
+	model.add(Activation('relu'))
+	model.add(Dropout(0.5))
+	model.add(Dense(num_classes))
+	model.add(Activation('softmax'))
+
+#	model.add(Convolution2D(32,3,3, input_shape=(64,64,3), activation='relu',\
+#	border_mode='same',dim_ordering='tf'))
+#
+#	model.add(Convolution2D(32,3,3, activation='relu', border_mode='same',dim_ordering='tf'))
+#
+#	model.add(Maxpool())
+#
+#	model.add(Flatten())
+#	model.add(Relu(128))
+#	#model.add(Dropout(0.5))
+#	model.add(Dense(num_classes, activation='softmax'))
 
 	optim = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 	model.compile(optim, loss='categorical_crossentropy', metrics=['accuracy'])
